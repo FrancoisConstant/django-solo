dc62ddb068b0e18a5e783c3ca0bd644469086362
Francois Constant <francois.constant@gmail.com>
2015-10-30 18:20:50 +1100

    Django 1.9

--- solo/templatetags/solo_tags.py
+++ solo/templatetags/solo_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.db import models
 from django.utils.translation import ugettext as _
 
 from solo import settings as solo_settings
@@ -11,13 +10,19 @@ register = template.Library()
 @register.assignment_tag(name=solo_settings.GET_SOLO_TEMPLATE_TAG_NAME)
 def get_solo(model_path):
     try:
+        from django.apps import apps
+        get_model = apps.get_model
+    except ImportError:
+        from django.db.models.loading import get_model
+
+    try:
         app_label, model_name = model_path.rsplit('.', 1)
     except ValueError:
         raise template.TemplateSyntaxError(_(
             "Templatetag requires the model dotted path: 'app_label.ModelName'. "
             "Received '%s'." % model_path
         ))
-    model_class = models.get_model(app_label, model_name)
+    model_class = get_model(app_label, model_name)
     if not model_class:
         raise template.TemplateSyntaxError(_(
             "Could not get the model name '%(model)s' from the application "
